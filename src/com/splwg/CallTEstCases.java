b6f92a0c983895121e1badd45fa73f235672d93f
bhagwatamols <bhagwatamols@rediffmail.com>
2023-01-23 20:58:57 +0530

    Add files via upload

---
+++ src/com/splwg/CallTEstCases.java
@@ -0,0 +1,11 @@
+package com.splwg;
+
+import org.junit.platform.console.ConsoleLauncher;
+
+public class CallTEstCases {
+
+	
+	public static void main(String[] args) {
+		ConsoleLauncher.main(args);
+	}
+}
